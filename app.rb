eeb4d6de68f34930035e83a7899a13c1050c08a9
jessica stenning <jessicastenning@hotmail.co.uk>
2018-05-01 16:42:01 +0100

    adds form to input player names

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require 'sinatra'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
+  end
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
 end
