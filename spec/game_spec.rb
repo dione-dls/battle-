4e176f42f94e212db81d4955b719c655ff3d1923
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-05-03 14:36:13 +0100

    changed tests to include doubles for the Game and Player glass

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,15 +3,8 @@ require 'player'
 
 describe Game do
   subject(:game) { described_class.new }
-  subject(:player1) { Player.new('Charly') }
-  subject(:player2) { Player.new('Zoe') }
-  
-  describe '#damage_received' do
-    it 'reduces the hit points by damage amount' do
-      game.attack(player2)
-      expect(player2.hit_points).to eq 50
-    end
-  end
+  subject(:player1) { double :player }
+  subject(:player2) { double :player }
 
   describe '#attack' do
     it 'should reduce the other player\'s hit points on attack' do
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,9 @@
 require 'player'
+require 'game'
 
 describe Player do
   subject(:name) { described_class.new('Zoe') }
+  subject(:game) { Game.new }
   subject(:player1) { described_class.new('Charly') }
   subject(:player2) { described_class.new('Zoe') }
 
@@ -16,6 +18,14 @@ describe Player do
       it 'should return the player\'s hit points' do
         allow(subject).to receive(:hit_points) { Player::HIT_POINTS }
         expect(subject.hit_points).to eq Player::HIT_POINTS
+    end
+  end
+
+  describe '#damage_received' do
+    it 'reduces the hit points by damage amount' do
+      allow(player1).to receive(:damage_received)
+      game.attack(player2)
+      expect(player2.hit_points).to eq 50
     end
   end
 end
