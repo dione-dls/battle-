d226620020a72e2e0a9aa7e9be509fb116deb4ac
Asia Antczak <aasiaantczak@gmail.com>
2018-05-04 14:28:47 +0100

    refactoring

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,15 +1,15 @@
 feature 'attack' do
   scenario "attack player 2" do
-    p sign_in_and_play
-    p click_button('Attack')
-    p expect(page).to have_content "Sally attacked Bob"
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to have_content "Sally attacked Bob"
   end
 
   scenario 'player 2 received hit and loses points' do
     sign_in_and_play
     click_button('Attack')
     click_link 'OK'
-    expect(page).not_to have_content "Bob 60HP"
     expect(page).to have_content "Bob 50HP"
   end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,13 +3,13 @@ require 'player'
 
 describe Game do
   subject(:game) { described_class.new(player1, player2) }
-  subject(:player1) { double :player }
-  subject(:player2) { double :player }
+
+  let(:player1) { double :player1 }
+  let(:player2) { double :player2, hit_points: 60, damage_received: 50 }
 
   describe '#attack' do
-    it 'should reduce the other player\'s hit points on attack' do
-      expect(player2).to receive(:damage_received)
-      game.attack(player2)
+    it 'should reduce the player\'s 2 hit points on attack' do
+      expect(game.attack(player2)).to eq 50
     end
   end
 end
