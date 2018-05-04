bfd9cc68ef755268dc999d549def14d801165545
Asia Antczak <aasiaantczak@gmail.com>
2018-05-04 15:16:36 +0100

    show player 1 hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,12 @@
 feature 'viewing hit points' do
+
   scenario 'view player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content "Bob 60HP"
+  end
+
+  scenario 'view player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content "Sally 60HP"
   end
 end
